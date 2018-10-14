51a99f0179742c5ef864e2e01874083d042d6a49
Nikita Pyanykh <nickkomarowski@gmail.com>
2018-10-14 21:19:18 +0300

    Initial commit

---
+++ app/src/main/java/com/quizapp/nickp/naukaprawjazdy/AccountActivity/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.quizapp.nickp.naukaprawjazdy.AccountActivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.quizapp.nickp.naukaprawjazdy.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
